8fa994c73f65fcea94602829e5e6e3094d00f090
dattatreya05 <89331208+dattatreya05@users.noreply.github.com>
2021-10-17 15:32:00 +0530

    Update highlightsentence(q10).js

--- highlightsentence(q10).js
+++ highlightsentence(q10).js
@@ -1,3 +1,6 @@
+<!-- 10. In your, HTML page add the below line and Highlight it without using any CSS.
+"HTML & CSS is awesome"  -->
+
 let button = document.getElementById("track");
 
 button.addEventListener("mouseover", () => {});
