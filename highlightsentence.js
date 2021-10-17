a80bd8ddfe9d97ee795f9b43f014ac9e07aa8556
dattatreya05 <89331208+dattatreya05@users.noreply.github.com>
2021-10-17 15:31:04 +0530

    Create highlightsentence.js

---
+++ highlightsentence.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("track");
+
+button.addEventListener("mouseover", () => {});
+
+let para = document.getElementById("para");
+para.addEventListener("mouseover", () => {
+    para.style.backgroundColor = "chartreuse";
+});
